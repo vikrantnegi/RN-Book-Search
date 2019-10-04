c1517a16defec6cf5739e83fd5032c410c945fce
vikrantnegi <vikrantnegi007@gmail.com>
2019-10-04 18:32:57 +0530

    feat: add search icon and onPress logic

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -7,7 +7,9 @@ import {
   View,
   Text,
   StyleSheet,
+  Platform,
 } from 'react-native';
+import * as Icon from '@expo/vector-icons';
 
 import {
   Body,
@@ -26,16 +28,50 @@ export default class SearchScreen extends React.Component {
     text: '',
   };
 
+  searchBooks = () => {
+    this.props.navigation.navigate('Results', {
+      searchQuery: this.state.text,
+    });
+  };
+
   render() {
     return (
-      <Wrapper normal center style={{}}>
+      <Wrapper normal center>
         <Logo />
-        <Input
-          medium
-          value={this.state.text}
-          placeholder="Search Books..."
-          onChangeText={changedText => this.setState({ text: changedText })}
-        />
+        <View
+          style={{
+            flexDirection: 'row',
+          }}
+        >
+          <Input
+            medium
+            value={this.state.text}
+            placeholder="Search Books..."
+            onChangeText={changedText => this.setState({ text: changedText })}
+            style={{
+              flex: 1,
+              paddingRight: 45,
+            }}
+          />
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              right: 0,
+              top: 0,
+              bottom: 0,
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            onPress={this.searchBooks}
+          >
+            <Icon.Ionicons
+              name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'}
+              size={30}
+              style={{ marginBottom: -3 }}
+            />
+          </TouchableOpacity>
+        </View>
       </Wrapper>
     );
   }
