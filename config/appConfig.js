2f17ef5cb26975777c5c8f4e8b94c9913ac0145e
vikrantnegi <vikrantnegi007@gmail.com>
2019-10-05 14:36:10 +0530

    refactor: removed unused variables

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,9 +1,6 @@
 const AppConfig = {
   apiEndPoint: 'https://www.googleapis.com/books/v1/volumes',
   defaultImageUrl: 'https://i.ibb.co/YLC0nQQ/not-found.png',
-  lottieAnimationSources: {
-    bookAnimation: require('../assets/RNBookSearch.json'),
-  },
 };
 
 export default AppConfig;
