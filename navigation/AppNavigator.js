b7b67387516e24b2f1fc3a5f5337fe73d4d94e24
vikrantnegi <vikrantnegi007@gmail.com>
2019-10-09 10:26:25 +0530

    feat: add drawer navigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,20 +1,39 @@
-import { createAppContainer } from 'react-navigation';
+import React from 'react';
+import { createAppContainer, DrawerItems } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { Ionicons } from '@expo/vector-icons';
+
 import SearchScreen from '../screens/SearchScreen';
 import ResultScreen from '../screens/ResultScreen';
+import BookDetailScreen from '../screens/BookDetailScreen';
 
-const AppNavigator = createStackNavigator(
-  {
-    Search: {
-      screen: SearchScreen,
-    },
-    Result: {
-      screen: ResultScreen,
-    },
+const MainStack = createStackNavigator({
+  Search: {
+    screen: SearchScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: null,
+      headerTransparent: true,
+      headerLeft: (
+        <Ionicons
+          name="md-menu"
+          size={26}
+          style={{ marginLeft: 10, padding: 10 }}
+          onPress={() => navigation.openDrawer()}
+        />
+      ),
+    }),
   },
-  {
-    headerMode: 'none',
-  }
-);
+  Result: {
+    screen: ResultScreen,
+  },
+  BookDetail: {
+    screen: BookDetailScreen,
+  },
+});
 
-export default createAppContainer(AppNavigator);
+const RootStack = createDrawerNavigator({
+  Main: MainStack,
+});
+
+export default createAppContainer(RootStack);
